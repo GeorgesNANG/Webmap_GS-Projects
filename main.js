489b9081f75eb41f1e3ed4c4b85bcc79ee1eea4d
GeorgesNANG <georgesmarvin99@gmail.com>
2024-06-06 18:27:09 +0100

    Code for a webmap of GS projects

---
+++ main.js
@@ -0,0 +1,24 @@
+// Initialize the map
+var map = L.map('map').setView([latitude, longitude], zoomLevel);
+
+// Add a basemap (e.g., OpenStreetMap)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Load your shapefile data (replace with your own GeoJSON URL)
+var shapefileUrl = 'path/to/your/shapefile.geojson';
+
+// Fetch the GeoJSON data
+fetch(shapefileUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Create a GeoJSON layer and add it to the map
+    L.geoJSON(data, {
+      onEachFeature: function (feature, layer) {
+        // Customize pop-up content (use properties from your shapefile)
+        layer.bindPopup('<b>' + feature.properties.name + '</b><br>' + feature.properties.description);
+      }
+    }).addTo(map);
+  })
+  .catch(error => console.error('Error loading shapefile:', error));
